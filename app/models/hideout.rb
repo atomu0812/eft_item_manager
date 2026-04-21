77f544854e2d7a6b12436b0370ea211df4b31af4
atomu0812 <kaokamoto@icloud.com>
2026-04-21 18:37:51 +0900

    Create hideouts table

---
+++ app/models/hideout.rb
@@ -0,0 +1,2 @@
+class Hideout < ApplicationRecord
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2026_04_21_043953) do
+ActiveRecord::Schema[7.1].define(version: 2026_04_21_093610) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "hideouts", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "items", force: :cascade do |t|
     t.string "name"
