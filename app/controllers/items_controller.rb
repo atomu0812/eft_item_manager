415ffa85d7be4051373219aabbeb93057882e10a
atomu0812 <kaokamoto@icloud.com>
2026-04-21 14:23:52 +0900

    Implement item quantity comparison display

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,12 @@
 class ItemsController < ApplicationController
   def index
     @items = Item.all
+
+    if user_signed_in?
+      @user_items_by_item_id = current_user.user_items.index_by(&:item_id)
+    else
+      @user_items_by_item_id = {}
+    end
   end
 
   def show
