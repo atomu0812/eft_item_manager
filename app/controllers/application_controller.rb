114f989eb25437df559599ef733090638cd52250
atomu0812 <kaokamoto@icloud.com>
2026-05-09 19:37:05 +0900

    管理者権限を追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,4 +12,10 @@ class ApplicationController < ActionController::Base
   def after_sign_out_path_for(resource_or_scope)
     root_path
   end
+
+  def require_admin!
+    return if user_signed_in? && current_user.admin?
+
+    redirect_to root_path, alert: "管理者のみアクセスできます。"
+  end
 end
