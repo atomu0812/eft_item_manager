d6af32813dd73eb56bd844f2a06cc5e090f0bf25
atomu0812 <kaokamoto@icloud.com>
2026-05-12 02:36:54 +0900

    タスク進行状況の絞り込み機能を追加

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,6 +2,7 @@ class TasksController < ApplicationController
   def index
     @selected_trader = params[:trader].to_s.strip
     @selected_level = params[:level].to_s.strip
+    @selected_status = params[:status].to_s.strip
 
     @traders = Task.where.not(trader: [nil, ""]).distinct.order(:trader).pluck(:trader)
     @levels = Task.distinct.order(:level).pluck(:level)
@@ -9,6 +10,21 @@ class TasksController < ApplicationController
     @tasks = Task.order(:name)
     @tasks = @tasks.where(trader: @selected_trader) if @selected_trader.present?
     @tasks = @tasks.where(level: @selected_level.to_i) if @selected_level.present?
+
+    if user_signed_in?
+      @user_tasks_by_task_id = current_user.user_tasks.index_by(&:task_id)
+
+      case @selected_status
+      when "completed"
+        completed_task_ids = current_user.user_tasks.where(completed: true).pluck(:task_id)
+        @tasks = @tasks.where(id: completed_task_ids)
+      when "incomplete"
+        completed_task_ids = current_user.user_tasks.where(completed: true).pluck(:task_id)
+        @tasks = @tasks.where.not(id: completed_task_ids)
+      end
+    else
+      @user_tasks_by_task_id = {}
+    end
   end
 
   def show
