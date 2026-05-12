e2dcbb01f61cb0fe88f2f99113b53d4dc4519510
atomu0812 <kaokamoto@icloud.com>
2026-05-13 04:37:23 +0900

    ログイン状態保持機能とトップページ表示を調整

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,11 @@ class ApplicationController < ActionController::Base
   protected
 
   def after_sign_in_path_for(resource)
-    items_path
+    root_path
   end
 
   def after_sign_up_path_for(resource)
-    items_path
+    root_path
   end
 
   def after_sign_out_path_for(resource_or_scope)
@@ -14,8 +14,8 @@ class ApplicationController < ActionController::Base
   end
 
   def require_admin!
-    return if user_signed_in? && current_user.admin?
-
-    redirect_to root_path, alert: "管理者のみアクセスできます。"
+    unless user_signed_in? && current_user.admin?
+      redirect_to root_path, alert: "管理者のみアクセスできます。"
+    end
   end
 end
